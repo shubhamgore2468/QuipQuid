95010ab773bab10d179e7d2b18e6f8cb31dd870c
shubhamgore2468 <shubhamgore2468@gmail.com>
2025-05-03 21:04:12 -0700

    added db connections

---
+++ backend/app/core/config.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+class Settings(BaseSettings):
+    #API Conifg
+    API_STR: str = '/api'
+    PROJECT_NAME:str = "AI Budget Tracker"
+
+    # Database URLs
+    POSTGRESQL_URL: str = Field(..., env="POSTGRESQL_URL")
+    MONGODB_URL: str = Field(..., env="MONGODB_URL")
+    # NEO4J_URL: str = Field(..., env="NEO4J_URL")
+    # NEO4J_USER: str = Field(..., env="NEO4J_USER")
+    # NEO4J_PASSWORD: str = Field(..., env="NEO4J_PASSWORD")
+
+    ANTHROPIC_API_KEY: str = Field(..., env="ANTHROPIC_API_KEY")
+
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+settings = Settings()
---
+++ backend/app/db/mongodb.py
@@ -0,0 +1,20 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+#motor connects on demand
+class MongoDB:
+    client: AsyncIOMotorClient = None
+    @classmethod
+    async def connect_to_mongo(cls):
+        cls.client = AsyncIOMotorClient(settings.MONGODB_URL)
+
+    @classmethod
+    async def close_mongo_connection(cls):
+        cls.client.close()
+
+    @classmethod
+    def get_database(cls):
+        return cls.client.budget_tracker
+
+    @classmethod
+    def get_chat_collections(cls):
+        return cls.get_database().chat_history
