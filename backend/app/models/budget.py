adde86b0502affb4965a868ff3b880f34e73c77a
Laukik Avhad <laukik28avhad@gmail.com>
2025-05-04 18:07:58 -0700

    sql update goal & budget

--- backend/app/models/budget.py
+++ backend/app/models/budget.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, Date, Enum
+from sqlalchemy import Column, Integer, String, Float, DateTime, Date, Enum, ForeignKey
 from sqlalchemy.sql import func
 from db.postgresql import Base
-from expense import ExpenseCategory
+from models.expense import ExpenseCategory
 
 class Budget(Base):
     __tablename__ = 'budgets'
     
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     expense_category = Column(Enum(ExpenseCategory, values_callable=lambda x: [e.value for e in x]), nullable=False)
     percentage = Column(Float, nullable=True)
     amount = Column(Float, nullable=False)
